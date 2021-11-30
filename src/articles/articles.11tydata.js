a30a3da3587719f5bcfb102e7567090d4719f0ca
ivanange <ivanangethe1@gmail.com>
2021-11-30 17:53:05 +0100

    updated tutors info

--- src/articles/articles.11tydata.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    // eleventyComputed: {
-    //     title: data => data.page.fileSlug.replace(/(-|_)/g, " ")
-    // },
-    layout: 'layouts/articles.njk',
-    tags: ['article'],
-    name: 'article',
-};
